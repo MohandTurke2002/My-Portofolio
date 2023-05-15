665fc7328331815ccee6c3a3a93d9d5ef46b8f47
mohandturky2002 <mohandhany35@gmail.com>
2023-05-15 22:47:30 +0300

    finish contact me form

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,40 +1,64 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import Settings from "../components/Settings/Settings";
 import Navbar from "../components/NavBar/Navbar";
+import "./scss/contactme.scss";
 
 function ContactMe() {
   const form = useRef();
+  const name = useRef();
+  const email = useRef();
+  const message = useRef();
+  const [error, setError] = useState(false);
+  const [messageError, setMessageError] = useState("");
+  const [done, setDone] = useState(false);
+  const [messageDone, setMessageDone] = useState("");
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_r19f6bg",
-        "template_meslrhg",
-        form.current,
-        "EQXhGgOYd7I4Z0L2l"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    if (!name.current.value) {
+      setError(true);
+      setMessageError("I Need Your Name");
+    } else if (!email.current.value) {
+      setError(true);
+      setMessageError("I Need Your Mail");
+    } else if (!message.current.value) {
+      setError(true);
+      setMessageError("Write Your Message ");
+    } else {
+      setError(false);
+      setDone(true);
+      setMessageDone("Done");
+      emailjs
+        .sendForm(
+          "service_r19f6bg",
+          "template_meslrhg",
+          form.current,
+          "EQXhGgOYd7I4Z0L2l"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    }
   };
   return (
-    <div>
+    <div className="container">
       <Settings />
       <div className="contact-me">
         <h1 className="special-title">
           Get In <span className="change-color">Touch</span>
         </h1>
+        {error && <p className="message-error">{messageError}</p>}
+        {done && <p className="message-done">{messageDone}</p>}
         <div className="container-form">
           <div className="social-links">
             <h3>Don't be shy!</h3>
-            <p>
+            <p className="my-message">
               Feel free to get in touch with me, I'm always open to discussing
               new projects and help you to solve any Errors or bugs{" "}
             </p>
@@ -60,35 +84,70 @@ function ContactMe() {
             </div>
             <div className="social">
               <div>
-                <a href="/">
+                <a
+                  href="https://www.facebook.com/mohand.hany.9210?mibextid=ZbWKwL"
+                  target="blank"
+                >
                   <i className="bi bi-facebook"></i>
                 </a>
               </div>
               <div>
-                <a href="/">
+                <a href="https://wa.me/qr/ABGM33YOZPYZB1" target="blank">
                   <i className="bi bi-whatsapp"></i>
                 </a>
               </div>
               <div>
-                <a href="/">
+                <a href="https://github.com/MohandTurke2002" target="blank">
                   <i className="bi bi-github"></i>
                 </a>
               </div>
               <div>
-                <a href="/">
+                <a
+                  href="https://www.instagram.com/invites/contact/?i=rliwdl3rqj51&utm_content=2r04jwa"
+                  target="blank"
+                >
                   <i className="bi bi-instagram"></i>
                 </a>
               </div>
             </div>
           </div>
           <form ref={form} onSubmit={sendEmail}>
-            <label>Name</label>
-            <input type="text" name="user_name" />
-            <label>Email</label>
-            <input type="email" name="user_email" />
-            <label>Message</label>
-            <textarea name="message" />
-            <input type="submit" value="Send" />
+            <div className="input-container">
+              <div className="input">
+                <input
+                  type="text"
+                  name="user_name"
+                  placeholder="Your Name"
+                  ref={name}
+                />
+              </div>
+              <div className="input">
+                <input
+                  type="email"
+                  name="user_email"
+                  placeholder="Your Email"
+                  ref={email}
+                />
+              </div>
+              <div className="input">
+                <input type="text" placeholder="Subject" />
+              </div>
+            </div>
+            <div className="message-text">
+              <textarea
+                name="message"
+                placeholder="Your Message here..."
+                ref={message}
+              />
+            </div>
+            <div className="submit">
+              <button className="change-bordercl">
+                Send Message
+                <span>
+                  <i className="bi bi-send"></i>
+                </span>
+              </button>
+            </div>
           </form>
         </div>
       </div>
